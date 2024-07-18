f473e640cbe881888a19bd81bc0dda2bf627d098
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-07-18 19:09:35 +0200

    Add shuffle simbols array Attached symbols to cards' back

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,10 @@
 const cardContainer = document.getElementById('cardContainer');
 const numberOfCells = 12;
 const startBtn = document.getElementById('startBtn');
+// Array di simboli per il back delle memory_card
+const symbols = ['☄️', '🚀', '🛸', '🪐', '👽', '💫'];
 
+// Creo le celle per la table
 for (let i = 0; i < numberOfCells; i++) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
@@ -10,12 +13,20 @@ for (let i = 0; i < numberOfCells; i++) {
 }
 
 // Aggiungi un listener per il click
+
 startBtn.addEventListener('click', function () {
 
+    // Duplico gli array
+    const symbolsDoble = [...symbols, ...symbols];
+    console.log(symbolsDoble);
+    // Mischio l'arrey combinato per randomizzare l'ordine -> funzione 
+    const symbolsShuffled = randomizerArraySimb(symbolsDoble)
+    console.log(symbolsShuffled);
+    // Assegno il simbolo random alla carta
 
     // Genera dinamicamente le memory_card
     const cells = document.querySelectorAll('.cell');
-    cells.forEach(cell => {
+    cells.forEach((cell, index) => {
 
         // Rimuove il contenuto precedente della cella
         cell.innerHTML = '';
@@ -30,6 +41,8 @@ startBtn.addEventListener('click', function () {
 
         const back = document.createElement('div');
         back.classList.add('back');
+        back.textContent = symbolsShuffled[index];
+
 
         memoryCard.appendChild(front);
         memoryCard.appendChild(back);
@@ -37,3 +50,14 @@ startBtn.addEventListener('click', function () {
         cell.appendChild(memoryCard);
     });
 });
+
+// Function
+
+function randomizerArraySimb(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const random = Math.floor(Math.random() * (i + 1));
+        [array[i], array[random]] = [array[random], array[i]];
+    }
+    // console.log(array);
+    return array;
+}
